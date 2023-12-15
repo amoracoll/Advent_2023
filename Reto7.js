9781ba7ff8d151c3549d5ae536e7a887486abfff
DESKTOP-38JM5KT\arnau.mora_modelical <arnaumoracoll@gmail.com>
2023-12-15 16:16:09 +0100

    compromiso

---
+++ Reto7.js
@@ -0,0 +1,14 @@
+/* Santa está experimentando con nuevos diseños de regalos y necesita tu ayuda para visualizarlos en 3D.
+Tu tarea es escribir una función que, dado un tamaño n (entero), genere un dibujo de un regalo en 3D utilizando caracteres ASCII.
+Las líneas de los regalos se dibujan con # y las caras con el símbolo que nos pasan como parámetro:
+drawGift(4, '+')
+
+/*
+   ####
+  #++##
+ #++#+#
+####++#
+#++#+#
+#++##
+####
+*/
