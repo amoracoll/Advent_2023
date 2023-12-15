16c42fcd971a1d5903ec4afa41d59179007f3592
DESKTOP-38JM5KT\arnau.mora_modelical <arnaumoracoll@gmail.com>
2023-12-15 18:48:19 +0100

    reto7

--- Reto7.js
+++ Reto7.js
@@ -2,6 +2,9 @@
 Tu tarea es escribir una función que, dado un tamaño n (entero), genere un dibujo de un regalo en 3D utilizando caracteres ASCII.
 Las líneas de los regalos se dibujan con # y las caras con el símbolo que nos pasan como parámetro:
 drawGift(4, '+')
+*/
+
+drawGift(4, '+')
 
 /*
    ####
@@ -12,3 +15,33 @@ drawGift(4, '+')
 #++##
 ####
 */
+drawGift(5, '*')
+/*
+    #####
+   #***##
+  #***#*#
+ #***#**#
+#####***#
+#***#**#
+#***#*#
+#***##
+#####
+*/
+
+drawGift(1, '^')
+/*
+#
+*/
+
+function drawGift(size, symbol){
+   if (size === 0){
+      return "Datos introducidos nulos";
+   }
+   else if (size === 1){
+      return `/*
+      ${symbol}
+      /*`;
+   }
+}
+
+console.log(drawGift(1,"@"));
