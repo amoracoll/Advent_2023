83fb8cac50166eb3054a7377da6e3bcf9548fa63
DESKTOP-38JM5KT\arnau.mora_modelical <arnaumoracoll@gmail.com>
2023-12-15 18:25:27 +0100

    Reto3 finalizado

--- Reto3.js
+++ Reto3.js
@@ -5,14 +5,25 @@ Tienes la secuencia original de pasos en la fabricación original y la secuencia
 Tu tarea es escribir una función que identifique y devuelva el primer paso extra que se ha añadido o eliminado en la cadena de fabricación. Si no hay ninguna diferencia entre las secuencias, devuelve una cadena vacía.
 */
 
-const original = "arnau";
-const modified = "arnau1";
+const original = "arnau2";
+const modified = "arnau";
 
 function findNaughtyStep(original, modified) {
-    if (original === modified){
-      return "Las dos cadenas de texto son iguales";
-    }else{
-      for (let i = 0; i < original.lenght )
+  if (original === modified) {
+    return "Las dos cadenas de texto son iguales";
+  } else {
+    let diferencia = [];
+    for (let i = 0; i < original.length || i < modified.length; i++) {
+      if (original[i] !== modified[i]) {
+        if (original.length > modified.length) {
+          diferencia.push(original[i]);
+        } else {
+          diferencia.push(modified[i]);
+        }
+      }
     }
-    return
+    return diferencia;
   }
+}
+
+console.log(findNaughtyStep(original, modified));
